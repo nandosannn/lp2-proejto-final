6e55bb7434b0a3b8d49977f560abdd5e2111f92e
nandosannn <silvafernandosan@gmail.com>
2025-01-22 22:06:03 -0300

    final

--- src/App.java
+++ src/App.java
@@ -8,7 +8,6 @@ import util.Util;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
 
         Connection conn = DB.getConnection();
         Scanner input = new Scanner(System.in);
